868ab70ced06c1c5a4c2b30ceeef485e501ff2b3
MrGrease <mehmetemreyorulmaz@gmail.com>
2020-04-20 14:44:28 +0300

    Added node modules and set up basic structure

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 const express = require('express');
-
+const path = require('path');
+const hbs = require('hbs');
+require('./db/db');
 const app = express();
 
+//Define paths
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+//setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+//Static directory path
+app.use(express.static(publicDirectoryPath));
+
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.render('index', {});
 });
 
 app.listen(process.env.PORT);
