65c3a34dc4d0cda09b20feefdb0f20e5f099fb4c
MrGrease <mehmetemreyorulmaz@gmail.com>
2020-05-18 17:02:17 +0300

    added different types of squeaks (text,link,video,image)

--- src/routers/user.js
+++ src/routers/user.js
@@ -127,10 +127,13 @@ router.get('/:id', async (req, res) => {
   }
 });
 //Get Home
-router.get('/', auth, async (req, res) => {
-  //res.redirect('/' + req.user._id);
-  res.render('home');
-});
+router.get(
+  '/',
+  /*auth,*/ async (req, res) => {
+    //res.redirect('/' + req.user._id);
+    res.render('home');
+  }
+);
 //Get likes
 router.get('/:id/likes', async (req, res) => {
   try {
